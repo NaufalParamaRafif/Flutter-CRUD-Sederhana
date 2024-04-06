2548b30db93302d44d905b888e34a65970f1125b
NaufalParamaRafif <profesoraiueo@gmail.com>
2024-04-06 20:59:56 +0700

    initialize repository

---
+++ README.md
@@ -0,0 +1,3 @@
+# crud_sederhana_flutter
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/crud_sederhana_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.crud_sederhana_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
